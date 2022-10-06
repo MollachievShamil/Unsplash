20c7c9fa95e8ab2a1090e1d20b294ac2ea0ac226
MollachievShamil <mollachiev.shamil@yandex.ru>
2022-10-06 15:05:31 +0700

    базовый дизайн для основного экрана коллекшн вью с пустышками, ячейка под картинку, серчало бар

--- Unsplash/Navigation/Controllers/MainVC/MainViewController.swift
+++ Unsplash/Navigation/Controllers/MainVC/MainViewController.swift
@@ -8,15 +8,88 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
-    var presenter: MainPresenterProtocol?
+    
+    var presenter: MainPresenterProtocol!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .green
+        setDelegate()
+        setConstraints()
+        setupSearchController()
+    }
+
+    let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.register(MainViewControllerCell.self, forCellWithReuseIdentifier: "cell")
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
+    
+    private let searchController = UISearchController(searchResultsController: nil)
+    
+    private func setupSearchController() {
+        searchController.searchBar.placeholder = "Search"
+        searchController.obscuresBackgroundDuringPresentation = false
+        navigationItem.searchController = searchController
+    }
+    
+    // MARK: - Delegates
+    func setDelegate() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        searchController.searchBar.delegate = self
     }
 }
 
+// MARK: - Presenter Delegate
 extension MainViewController: MainViewProtocol {
     
+}
+
+// MARK: - Collection View Delegates
+extension MainViewController: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MainViewControllerCell
+        cell.imageView.image = UIImage(systemName: "trash")
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        sizeForItemAt indexPath: IndexPath) -> CGSize {
+        CGSize(
+            width: (view.frame.size.width / 2) - 12,
+            height: (view.frame.size.width / 2) - 12
+        )
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,
+                        insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 6, left: 6, bottom: 6, right: 6)
+    }
+}
+
+    // MARK: - Search Bar Delegate
+extension MainViewController: UISearchBarDelegate {
+    
+}
+
+// MARK: - Constraints
+extension MainViewController {
+    func setConstraints() {
+        
+        view.addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: view.topAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
---
+++ Unsplash/Navigation/Controllers/MainVC/MainViewControllerCell.swift
@@ -0,0 +1,40 @@
+//
+//  MainViewControllerCell.swift
+//  Unsplash
+//
+//  Created by Шамиль Моллачиев on 06.10.2022.
+//
+
+import UIKit
+
+class MainViewControllerCell: UICollectionViewCell {
+
+    var imageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setConstraints()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func setConstraints() {
+        self.addSubview(imageView)
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
+            imageView.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 0),
+            imageView.trailingAnchor.constraint(equalTo: self.trailingAnchor, constant: 0),
+            imageView.bottomAnchor.constraint(equalTo: self.bottomAnchor, constant: 0),
+        ])
+    }
+}
+
